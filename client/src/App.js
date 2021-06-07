7aeb888a6b471629366e8c280e25933bc0987287
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-06-06 22:32:06 -0600

    App page routing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Router>
+  <div>
+    <Route exact path="/" component={Dashboard} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/signup" component={Signup} />
+  </div>
+</Router>
   );
 }
 
