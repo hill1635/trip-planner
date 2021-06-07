aa7ed6c5925719af8a3a70aebef91ee37dfb602b
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-06-06 21:24:37 -0600

    Deleted unnecessary files.  Added mongoose connect to server file.

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/trip-planner");
+
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
