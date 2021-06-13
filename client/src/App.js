e08e4f4a16dd4bc973e9dc119f6b1f9b20ead046
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-06-13 00:41:00 -0400

    Deleted unnecessary code and files.

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
-// import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import Footer from "./components/Footer";
 import 'bulma/css/bulma.min.css';
 import './App.scss';
@@ -14,8 +12,6 @@ function App() {
   <div>
     <Navbar />
     <Route exact path="/" component={Dashboard} />
-    {/* <Route exact path="/login" component={Login} /> */}
-    <Route exact path="/signup" component={Signup} />
     <Footer />
   </div>
 </Router>
