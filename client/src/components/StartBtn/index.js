83f82584f3cdc9c22b15a20886d95740be1e47f1
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-06-13 01:18:01 -0400

    Stylesheet imported.  Basic StartBtn styling.

--- client/src/components/StartBtn/index.js
+++ client/src/components/StartBtn/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./style.scss";
 
 function StartBtn() {
-  return <button>Start a New Journey</button>;
+  return (
+    <button className="startBtn">
+      <i class="fas fa-plus plus"></i>
+      Start a New Journey
+    </button>
+  );
 }
 
 export default StartBtn;
