585944c719653cc5e150992e0ae2aa2bd114d2f8
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-06-13 02:29:56 -0400

    Inputs and buttons added to NewTripModal.

--- client/src/components/NewTripModal/index.js
+++ client/src/components/NewTripModal/index.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.scss";
 
 function NewTripModal() {
-    return (<div></div>);
+  const [state, setState] = useState("modal newTripModal");
+
+  useEffect(() => {
+    var startBtn = document.querySelector(".startBtn");
+    startBtn.addEventListener("click", () => {
+      setState("modal newTripModal is-active");
+    });
+  });
+
+  function close() {
+    setState("modal newTripModal");
+  }
+
+  return (
+    <div className={state}>
+      <div className="modal-background"></div>
+      <div className="modal-card newTripBox">
+        <div className="modal-header">
+          <h1 className="title">New Journey</h1>
+        </div>
+        <div className="modal-content">
+            <p>Starting Location:</p>
+            <input type="text"></input>
+            <p>Ending Location:</p>
+            <input type="text"></input>
+            <p>Loop</p>
+            <input type="checkbox"></input>
+            <p>Start Date:</p>
+            <input type="text"></input>
+            <p>End Date:</p>
+            <input type="text"></input>
+            <p>Add Stop:</p>
+            <input type="text"></input>
+            <button className="addStop">Add</button>
+            <button className="submit">Save</button>
+            <button className="cancelBtn" aria-label="close" onClick={close}>Cancel</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NewTripModal;
