4effd8ad885dad0ee2b6c8f83d21c5cdf9dc97e3
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-06-22 23:26:52 -0600

    Formatting.

--- client/src/components/NewTripModal/index.js
+++ client/src/components/NewTripModal/index.js
@@ -6,7 +6,13 @@ function NewTripModal() {
 
   useEffect(() => {
     var startBtn = document.querySelector(".startBtn");
+    var addBtn = document.querySelector(".addBtn");
+
     startBtn.addEventListener("click", () => {
+      setState("modal newTripModal is-active");
+    });
+
+    addBtn.addEventListener("click", () => {
       setState("modal newTripModal is-active");
     });
   });
@@ -23,21 +29,23 @@ function NewTripModal() {
           <h1 className="title">New Journey</h1>
         </div>
         <div className="modal-content">
-            <p>Start Date:</p>
-            <input type="text" className="startDate"></input>
-            <p>End Date:</p>
-            <input type="text" className="endDate"></input>
-            <p>Starting Location:</p>
-            <input type="text" className="startLocation"></input>
-            <p>Ending Location:</p>
-            <input type="text" className="endLocation"></input>
-            <p>Loop</p>
-            <input type="checkbox" className="loop"></input>
-            <p>Add Stop:</p>
-            <input type="text" className="addStop"></input>
-            <button className="addBtn">Add</button>
-            <button className="submitBtn">Save</button>
-            <button className="cancelBtn" aria-label="close" onClick={close}>Cancel</button>
+          <p>Start Date:</p>
+          <input type="text" className="startDate"></input>
+          <p>End Date:</p>
+          <input type="text" className="endDate"></input>
+          <p>Starting Location:</p>
+          <input type="text" className="startLocation"></input>
+          <p>Ending Location:</p>
+          <input type="text" className="endLocation"></input>
+          <p>Loop</p>
+          <input type="checkbox" className="loop"></input>
+          <p>Add Stop:</p>
+          <input type="text" className="addStop"></input>
+          <button className="addBtn">Add</button>
+          <button className="submitBtn">Save</button>
+          <button className="cancelBtn" aria-label="close" onClick={close}>
+            Cancel
+          </button>
         </div>
       </div>
     </div>
