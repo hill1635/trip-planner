1d91757ac50c562eb7d3eb32255521d23469c475
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-06-29 22:53:10 -0600

    Fixed map method.

--- client/src/components/AddedStops/index.js
+++ client/src/components/AddedStops/index.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./style.scss";
 
 function AddedStops(props) {
   return (
     <div className="columns">
-      {props.stops.map((stop) => {
+      {props.stops.map((stop) => (
         <div className="column">
           <p>{stop}</p>
-        </div>;
-      })}
+        </div>
+      ))}
     </div>
   );
 }
