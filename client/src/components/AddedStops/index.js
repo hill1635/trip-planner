c84cc60ab888e0f5fa6d7187ddbb87c164efa2e6
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-07-05 15:44:49 -0600

    Migrated AddBtn to AddedStops component.

--- client/src/components/AddedStops/index.js
+++ client/src/components/AddedStops/index.js
@@ -1,15 +1,21 @@
 import React from "react";
+import AddBtn from "../AddBtn";
 import "./style.scss";
 
 function AddedStops(props) {
   return (
-    <div className="columns">
-      {props.stops.map((stop) => (
-        <div className="column" key={stop}>
-          <p>{stop}</p>
-        </div>
-      ))}
+    <div className="column is-two-fifths centered">
+      <div className="columns">
+        {props.stops.map((stop) => (
+          <div className="column" key={stop}>
+            <p>{stop}</p>
+          </div>
+        ))}
+      </div>
+      <input type="text" placeholder="Add Stop" className="addInput input is-rounded"></input>
+      <AddBtn stops={props.stops} setStops={(e) => props.setStops(e)} />
     </div>
+      
   );
 }
 
--- client/src/components/NewTripModal/index.js
+++ client/src/components/NewTripModal/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import AddBtn from "../AddBtn";
 import AddedStops from "../AddedStops";
 import SubmitBtn from "../SubmitBtn";
 import CancelBtn from "../CancelBtn";
@@ -46,11 +45,7 @@ function NewTripModal() {
               </div>
             </div>
           </div>
-          <div className="column is-two-fifths centered">
-            <AddedStops stops={stops}/>
-            <input type="text" placeholder="Add Stop" className="addInput input is-rounded"></input>
-            <AddBtn stops={stops} setStops={(e) => setStops(e)} />
-          </div>
+          <AddedStops stops={stops} setStops={setStops}/>
           <SubmitBtn stops={stops} />
           <CancelBtn close={close} />
         </div>
