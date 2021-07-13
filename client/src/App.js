4abf860c54311270f75b0cef62c3ae5b134e83e3
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-07-13 11:36:45 -0600

    Results page imported to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
+import Results from "./pages/Results";
 import Footer from "./components/Footer";
 import 'bulma/css/bulma.min.css';
 import './App.scss';
@@ -12,6 +13,7 @@ function App() {
   <div>
     <Navbar />
     <Route exact path="/" component={Dashboard} />
+    <Route exact path="/results" component={Results} />
     <Footer />
   </div>
 </Router>
