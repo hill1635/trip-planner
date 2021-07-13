f051920a58d309e1c14ebfdc10f02ca4ecc1580c
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-07-13 11:01:08 -0600

    Forr loop pushing startLocation and endLocation pairings with saved stops.

--- client/src/components/SubmitBtn/index.js
+++ client/src/components/SubmitBtn/index.js
@@ -2,9 +2,9 @@ import React, { useEffect } from "react";
 import "./style.scss";
 
 function SubmitBtn(props) {
-
   useEffect(() => {
     var submitBtn = document.querySelector(".submitBtn");
+    var legs = [];
 
     submitBtn.addEventListener("click", () => {
       var trip = {
@@ -12,16 +12,19 @@ function SubmitBtn(props) {
         endDate: document.querySelector(".endDate").value,
         startLocation: document.querySelector(".startLocation").value,
         endLocation: document.querySelector(".endLocation").value,
-        stops: props.stops
+        stops: props.stops,
       };
-      console.log("trip: ", trip);
+      for (let i = 0; i < trip.stops.length; i++) {
+        legs.push([trip.startLocation, trip.stops[i]]);
+        legs.push([trip.endLocation, trip.stops[i]]);
+      }
     });
   });
 
   return (
-  <button className="submitBtn button is-info is-rounded mr-3">
-    <i className="fas fa-save mr-1"></i>
-    Save
+    <button className="submitBtn button is-info is-rounded mr-3">
+      <i className="fas fa-save mr-1"></i>
+      Save
     </button>
   );
 }
