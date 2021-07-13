d26dc97d823747eeddda889a76cf662b54a06ac7
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-07-13 13:04:25 -0600

    Coordinates API route added.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export default {
-
+  searchCoordinates: function (search) {
+    return axios.get(
+      "https://api.mapbox.com/geocoding/v5/mapbox.places/" + search
+    );
+  },
 };
