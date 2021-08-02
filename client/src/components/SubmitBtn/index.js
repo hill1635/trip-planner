49b669ea644a34fdf074e8cf20ce70dffb3fe11f
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-08-02 14:39:28 -0600

    A.env API Key added to API.js.  Function added to submitBtn that converts stops into objects and gets coordinates.

--- client/src/components/SubmitBtn/index.js
+++ client/src/components/SubmitBtn/index.js
@@ -3,40 +3,51 @@ import API from "../../utils/API";
 import "./style.scss";
 
 function SubmitBtn(props) {
-  function renderCoordinates(name) {
+  function renderCoordinates(name, saveLocation) {
     API.searchCoordinates(name)
       .then((res) => {
-        console.log(res);
-        // saveLocation = {
-        //   name: name,
-        //   coordinates: {
-        //     lat: res.data,
-        //     lng: res.data
-        //   }
-        // }
+        saveLocation.coordinates = {
+          lat: res.data.features[0].geometry.coordinates[1],
+          lng: res.data.features[0].geometry.coordinates[0],
+        };
       })
       .catch((err) => console.log(err));
   }
 
   useEffect(() => {
     var submitBtn = document.querySelector(".submitBtn");
+    var stops = [];
 
+    
     submitBtn.addEventListener("click", () => {
+
+      props.stops.forEach((stop) => {
+        var stopObj = { 
+          name: stop,
+          coordinates: {}
+        };
+        renderCoordinates(stopObj.name, stopObj.coordinates);
+        stops.push(stopObj);
+      });
+
       var trip = {
         startDate: document.querySelector(".startDate").value,
         endDate: document.querySelector(".endDate").value,
         startLocation: {
           name: document.querySelector(".startLocation").value,
-          coordinates: { lat: "", lng: "" },
+          coordinates: {},
         },
         endLocation: {
           name: document.querySelector(".endLocation").value,
-          coordinates: { lat: "", lng: "" },
+          coordinates: {},
         },
-        stops: props.stops,
+        stops: stops,
         legs: [],
       };
-      renderCoordinates(trip.startLocation.name);
+
+      renderCoordinates(trip.startLocation.name, trip.startLocation);
+      renderCoordinates(trip.endLocation.name, trip.endLocation);
+
       for (let i = 0; i < trip.stops.length; i++) {
         trip.legs = [...trip.legs, [trip.startLocation, trip.stops[i]]];
         trip.legs = [...trip.legs, [trip.endLocation, trip.stops[i]]];
@@ -45,7 +56,7 @@ function SubmitBtn(props) {
         }
       }
       localStorage.setItem("newTrip", JSON.stringify(trip));
-      // window.location = "/results";
+      window.location = "/results";
     });
   });
 
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 var apiKey = process.env.REACT_APP_MAPBOX_API_KEY;
 
 export default {
-  searchCoordinates: function (search) {
+  searchCoordinates: function (search, saveLocation) {
     return axios.get(
       "https://api.mapbox.com/geocoding/v5/mapbox.places/" + search + ".json?&access_token=" + apiKey
     );
