0809d25927811f8990598078ace2417927435350
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-01-08 12:27:50 -0700

    RemoveBtn component created.

---
+++ client/src/components/NewTripModal/RemoveBtn/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.scss";
+
+function RemoveBtn() {
+  return (
+    <button>
+      <i className="fas fa-times mr-1"></i>
+    </button>
+  );
+}
+
+export default RemoveBtn;
