886ad2ec84d23e87616f2140e9f04bfaa57070c7
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-01-08 20:07:13 -0700

    Moved functions over to AddedStops.

--- client/src/components/NewTripModal/AddBtn/index.js
+++ client/src/components/NewTripModal/AddBtn/index.js
@@ -2,75 +2,7 @@ import React from "react";
 import "./style.scss";
 
 function AddBtn(props) {
-  var stops = [...props.stops];
-  var ordered = props.ordered;
-
-  function generateSRC(object) {
-    var googleMapsAPIKey = process.env.REACT_APP_GOOGLE_API_KEY;
-    var src =
-      "https://www.google.com/maps/embed/v1/directions?key=" + googleMapsAPIKey;
-    if (object.first !== null && object.last === null) {
-      src =
-        "https://www.google.com/maps/embed/v1/place?key=" +
-        googleMapsAPIKey +
-        "&q=" +
-        object.first;
-    }
-    if (object.last !== null) {
-      src += "&origin=" + object.first + "&destination=" + object.last;
-    }
-    if (object.waypoints.length > 0) {
-      src += "&waypoints=" + object.waypoints;
-    }
-    console.log(src);
-    props.setSRC(src);
-  }
- 
-  function convertFormat(object) {
-    var convertedObject = {
-      first: "",
-      last: "",
-      waypoints: "",
-    };
-    
-    if (object.first !== null) {
-      convertedObject.first = object.first.split(", ").join(",").split(" ").join("+");
-    }
-    if (object.last !== null) {
-      convertedObject.last = object.last.split(", ").join(",").split(" ").join("+");
-    }
-    if (object.waypoints.length > 0) {
-      console.log(object.waypoints);
-      convertedObject.waypoints = object.waypoints.join("|").split(", ").join(",").split(" ").join("+");
-    }
-    generateSRC(convertedObject);
-  }
-
-  function add() {
-    stops.push(props.input);
-    editStop();
-  }
-
-  function editStop() {
-    var newOrdered = ordered;
-    
-    stops.forEach((stop) => {
-      if (stops.indexOf(stop) === 0) {
-        newOrdered.first = stop;
-      } else if (stops.indexOf(stop) === stops.length - 1) {
-        newOrdered.last = stop;
-      } else {
-        if (!newOrdered.waypoints.includes(stop)) {
-        newOrdered.waypoints.push(stop);
-        }
-      }
-    });
-    console.log(newOrdered.waypoints);
-    props.setOrdered(newOrdered);
-    convertFormat(props.ordered);
-    props.setStops(stops);
-  }
-
+  var stops = props.stops;
   return (
     <button className="addBtn button is-rounded is-success" onClick={add}>
       <i className="fas fa-plus plus m-0 p-0"></i>
--- client/src/components/NewTripModal/AddedStops/index.js
+++ client/src/components/NewTripModal/AddedStops/index.js
@@ -10,8 +10,72 @@ function AddedStops(props) {
     waypoints: [],
     last: null,
   });
-
   var stops = [...props.stops];
+
+  function generateSRC(object) {
+    var googleMapsAPIKey = process.env.REACT_APP_GOOGLE_API_KEY;
+    var src =
+      "https://www.google.com/maps/embed/v1/directions?key=" + googleMapsAPIKey;
+    if (object.first !== null && object.last === null) {
+      src =
+        "https://www.google.com/maps/embed/v1/place?key=" +
+        googleMapsAPIKey +
+        "&q=" +
+        object.first;
+    }
+    if (object.last !== null) {
+      src += "&origin=" + object.first + "&destination=" + object.last;
+    }
+    if (object.waypoints.length > 0) {
+      src += "&waypoints=" + object.waypoints;
+    }
+    props.setSRC(src);
+  }
+ 
+  function convertFormat(object) {
+    var convertedObject = {
+      first: "",
+      last: "",
+      waypoints: "",
+    };
+    
+    if (object.first !== null) {
+      convertedObject.first = object.first.split(", ").join(",").split(" ").join("+");
+    }
+    if (object.last !== null) {
+      convertedObject.last = object.last.split(", ").join(",").split(" ").join("+");
+    }
+    if (object.waypoints.length > 0) {
+      convertedObject.waypoints = object.waypoints.join("|").split(", ").join(",").split(" ").join("+");
+    }
+    console.log(object);
+    generateSRC(convertedObject);
+  }
+
+  function editStops() {
+    var newOrdered = ordered;
+    
+    stops.forEach((stop) => {
+      if (stops.indexOf(stop) === 0) {
+        newOrdered.first = stop;
+      } else if (stops.indexOf(stop) === stops.length - 1) {
+        newOrdered.last = stop;
+      } else {
+        if (!newOrdered.waypoints.includes(stop)) {
+        newOrdered.waypoints.push(stop);
+        }
+      }
+    });
+    setOrdered(newOrdered);
+    convertFormat(ordered);
+    props.setStops(stops);
+  }
+
+  function add() {
+    props.setStops([...stops, props.input]);
+    editStops();
+  }
+
 
   return (
     <div>
@@ -23,7 +87,7 @@ function AddedStops(props) {
         </div>
       ))}
       <input type="text" placeholder="Add Destination" className="addDestination is-rounded" onChange={e => setInput(e.target.value)}></input>
-      <AddBtn stops={stops} setStops={props.setStops} input={input} setSRC={props.setSRC} ordered={ordered} setOrdered={setOrdered}/>
+      <AddBtn stops={stops} setStops={props.setStops} input={input} editStops={editStops}/>
     </div>
   );
 }
