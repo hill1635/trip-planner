791f364c487d17f2ceefe44ede3275cf1daa0902
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-01-09 21:20:13 -0700

    I forgot.

--- client/src/components/SignUpModal/index.js
+++ client/src/components/SignUpModal/index.js
@@ -7,11 +7,7 @@ function SignUpModal() {
 
     function submitSignup(target) {
       var submittedInfo = target.parentNode.children[0];
-      // var newUser = {
-      //   email: submittedInfo.children[0].children[0].value,
-      //   password: submittedInfo.children[1].children[0].value
-      // };
-      // console.log("newUser: ", newUser);
+      
       API.createUser({
         email: submittedInfo.children[0].children[0].value,
         password: submittedInfo.children[1].children[0].value
