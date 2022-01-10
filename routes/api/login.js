2702a0c12363897862974acb3f0309ffa0b3f3b7
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-01-09 22:39:33 -0700

    Router skeletons started.

--- routes/api/login.js
+++ routes/api/login.js
@@ -1,4 +1,13 @@
+const router = require("./users");
+
 router = require("express").Router();
 
+router
+    .route("/")
+    .post();
+
+router
+    .route("/")
+    .delete();
 
 module.exports = router;
