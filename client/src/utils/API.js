79d9338f5cd5a88e6557a1c6ce156e5c62859ccc
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-01-09 23:02:32 -0700

    Login axios function created.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -13,5 +13,8 @@ export default {
   },
   deleteUser: function(id) {
     return axios.delete("api/books/" + id);
-  }
+  },
+  login: function(userData) {
+    return axios.post("/api/users/login", userData);
+  },
 };
