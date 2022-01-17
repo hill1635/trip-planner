80de6dc1da757b358ea1b825f97b594f8eeb6459
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-01-17 11:55:09 -0700

    Added password hashing to create user function.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,9 @@
 const db = require("../models/user");
+const bcrypt = require("bcrypt");
 
 module.exports = {
   create: function (req, res) {
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
     db.create(req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
