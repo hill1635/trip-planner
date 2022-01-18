a3853c6159290fddcd6813aa3eaaafa6a5c31aee
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-01-17 18:10:51 -0700

    Mapbox API key variable deleted.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-var apiKey = process.env.REACT_APP_MAPBOX_API_KEY;
-
 export default {
   createUser: function(userData) {
     return axios.post("/api/users", userData);
