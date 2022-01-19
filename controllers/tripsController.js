eb8211f0bd332a700b65bbd725c5a1e56e1d1e48
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-01-18 17:43:31 -0700

    Create function added to tripController.

--- controllers/tripsController.js
+++ controllers/tripsController.js
@@ -1,5 +1,10 @@
 const db = require("../models/trip");
 
 module.exports = {
+    create: function (req, res) {
+        db.create(req.body)
+        .then((dbModel) => res.json(dbModel))
+        .catch((err) => res.status(422).json(err));
+    },
 
 };
