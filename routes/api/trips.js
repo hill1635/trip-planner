4e43342e0ba5948cecd4c5a8ca994e1f4f0f4f3d
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-01-18 19:01:11 -0700

    API routes created for trips.

--- routes/api/trips.js
+++ routes/api/trips.js
@@ -1,4 +1,16 @@
 const router = require("express").Router();
 const tripsController = require("../../controllers/tripsControllers");
 
+router
+    .route("/")
+    .post(tripsController.create);
+
+router
+    .route("/:id")
+    .delete(tripsController.remove);
+
+router
+    .route("/:id")
+    .put(tripsController.update);
+
 module.exports = router;
