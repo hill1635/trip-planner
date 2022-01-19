f4dabe89e95eb651900d8437edccd8a2cc5e2b7f
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-01-18 17:39:24 -0700

    Trip routes created.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
+const tripRoutes = require("./trips");
 
 router.use("/users", userRoutes);
+router.use("/trips", tripRoutes);
 
 module.exports = router;
