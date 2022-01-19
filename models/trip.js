4df3bb877e41f9c3e7c67de1c0904a4e3bc692e1
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2022-01-18 17:33:24 -0700

    Trip model properties added.

--- models/trip.js
+++ models/trip.js
@@ -2,7 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TripSchema = new Schema({
-
+  origin: {
+    type: String,
+  },
+  destination: {
+    type: String,
+  },
+  waypoints: {
+    type: Array,
+  },
+  createdBy: {
+    type: mongoose.ObjectId,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Trip = mongoose.model("Trip", TripSchema);
